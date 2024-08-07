04f6fee0c4caba6515dfe1bb4337fe1efb34d3b2
indcrediBro <152566488+indcrediBro@users.noreply.github.com>
2024-08-07 17:51:51 +0530

    Added Random Floor Generator and Grid Generator

---
+++ Assets/Scripts/FloorRandomiser.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorRandomiser : MonoBehaviour
+{
+    [SerializeField] private GameObject[] m_baseObjectsGO;
+    [SerializeField] private GameObject[] m_decorationsGO;
+
+    [SerializeField] private Transform m_baseParentTF;
+
+    [SerializeField] private float m_decorSpawnLocationRange;
+
+    private void Start()
+    {
+        SpawnRandomFloorTile();
+        RandomiseDecorations();
+    }
+
+    private void SpawnRandomFloorTile()
+    {
+        int randomIndex = RandomNumber.Instance.NextInt(m_baseObjectsGO.Length);
+
+        for (int i = 0; i < m_baseObjectsGO.Length; i++)
+        {
+            m_baseObjectsGO[i].SetActive(false);
+        }
+        m_baseObjectsGO[randomIndex].SetActive(true);
+
+        m_baseParentTF.localRotation = Quaternion.Euler(0f, 90f * RandomNumber.Instance.NextInt(4), 0f);
+    }
+
+    private void RandomiseDecorations()
+    {
+        int randomSpawnIndex = RandomNumber.Instance.NextInt(m_decorationsGO.Length + 50);
+
+        foreach (GameObject _decor in m_decorationsGO)
+        {
+            _decor.SetActive(false);
+        }
+
+        if (randomSpawnIndex < m_decorationsGO.Length)
+        {
+            GameObject decor = m_decorationsGO[randomSpawnIndex];
+            decor.SetActive(true);
+            decor.transform.localPosition = new Vector3(
+                                            RandomNumber.Instance.NextFloat(-m_decorSpawnLocationRange, m_decorSpawnLocationRange),
+                                            decor.transform.localPosition.y,
+                                            RandomNumber.Instance.NextFloat(-m_decorSpawnLocationRange, m_decorSpawnLocationRange)
+                                            );
+            decor.transform.localRotation = Quaternion.Euler(0f, RandomNumber.Instance.NextFloat(0f, 360f), 0f);
+            decor.transform.localScale = Vector3.one * RandomNumber.Instance.NextFloat(0.5f, 1.25f);
+        }
+    }
+}
