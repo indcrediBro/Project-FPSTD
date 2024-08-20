c0d8e8764dd622258f98b572e5d31c0bee54f5ad
indcrediBro <152566488+indcrediBro@users.noreply.github.com>
2024-08-21 01:16:28 +0530

    Minor bug fix for immersive trees

--- Assets/Scripts/Level/RotateTowardsPlayer.cs
+++ Assets/Scripts/Level/RotateTowardsPlayer.cs
@@ -8,14 +8,23 @@ public class RotateTowardsPlayer : MonoBehaviour
 
     private void Awake()
     {
-        m_player = GameReferences.Instance.m_PlayerStats.transform;
+        if(GameReferences.Instance) m_player = GameReferences.Instance.m_PlayerStats.transform;
     }
 
     private void LateUpdate()
     {
-        Vector3 direction = m_player.position - transform.position;
+        if (m_player) {
+            Vector3 direction = m_player.position - transform.position;
 
-        direction.y = 0;
-        transform.rotation = Quaternion.LookRotation(direction);
+            direction.y = 0;
+            transform.rotation = Quaternion.LookRotation(direction);
+        }
+        else
+        {
+            Vector3 direction = Camera.main.transform.position - transform.position;
+
+            direction.y = 0;
+            transform.rotation = Quaternion.LookRotation(direction);
+        }
     }
 }
